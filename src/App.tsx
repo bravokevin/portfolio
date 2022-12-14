a0baac9461fede4f20c84429ff81b42e815d6e6e
Kevin Bravo <79945749+mustafinho@users.noreply.github.com>
2022-12-14 20:20:20 +0000

    fix:

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => {
         <Resume />
         {/* <Portfolio/> */}
         <Blog/>
-        <Testimonials />
+        {/* <Testimonials /> */}
         <Contact />
       </main>
       <Footer />
